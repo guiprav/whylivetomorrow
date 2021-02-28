fc9d405069138936fe6dedb31115441d07e369fd
Gui Prá <gui@supersaas.com>
2021-03-01 00:21:19 +0100

    .

--- css.js
+++ css.js
@@ -11,24 +11,13 @@ function bem(block, els) {
 }
 
 let obs = new MutationObserver(muts => {
-  obs.disconnect();
-
   for (let mut of muts) {
     let name = mut.attributeName;
     if (name !== 'class') { continue }
 
-    let value = mut.target.getAttribute(name);
-    if (!/^[A-Z]/.test(value.trim())) { continue }
-
-    let [bemId, ...xs] = value.split(' ');
-
-    for (let el of document.querySelectorAll(`.${bemId}`)) {
-      tw(xs.join(' '));
-      el.className = value;
-    }
+    tw(mut.target.getAttribute(name)
+      .split(' ').filter(x => !/[A-Z]/.test(x)).join(' '));
   }
-
-  obs.observe(document.body, { attributes: true, attributeOldValue: true, subtree: true });
 });
 
 requestAnimationFrame(() => obs.observe(document.body, {
